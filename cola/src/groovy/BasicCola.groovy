96e94895e559ae30e85bf1f84feec11001587af9
J. David Beutel <git@getsu.com>
2012-04-19 21:32:24 -1000

    not really trying Grails

---
+++ cola/src/groovy/BasicCola.groovy
@@ -0,0 +1,11 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jdb
+ * Date: 4/18/12
+ * Time: 11:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class BasicCola {
+    List<List> level = []
+}
