5fcde89d5860f32d8b2191d9934e97814e3391f6
J. David Beutel <git@getsu.com>
2012-04-26 16:04:54 -1000

    griffin create-processing-view AmortizedCola
     also added library, processing as src dir, .gitignore, etc

---
+++ AmortizedCola/griffon-app/processing/amortizedcola/AmortizedColaProcessingView.java
@@ -0,0 +1,37 @@
+package amortizedcola;
+
+import griffon.plugins.processing.artifact.AbstractGriffonProcessingView;
+
+public class AmortizedColaProcessingView extends AbstractGriffonProcessingView {
+    public AmortizedColaModel model;
+    public AmortizedColaController controller;
+
+    public void setModel(AmortizedColaModel model) {
+        this.model = model;
+    }
+
+    public void setController(AmortizedColaController controller) {
+        this.controller = controller;
+    }
+
+    private float y = 100;
+
+    // The statements in the setup() function 
+    // execute once when the program begins
+    public void setup() {
+        size(200, 200);  // Size should be the first statement
+        stroke(255);     // Set line drawing color to white
+        frameRate(30);
+    }
+
+    // The statements in draw() are executed until the 
+    // program is stopped. Each statement is executed in 
+    // sequence and after the last line is read, the first 
+    // line is executed again.
+    public void draw() {
+        background(0);   // Set the background to black
+        y = y - 1; 
+        if (y < 0) { y = height; } 
+        line(0, y, width, y); 
+    }
+}
